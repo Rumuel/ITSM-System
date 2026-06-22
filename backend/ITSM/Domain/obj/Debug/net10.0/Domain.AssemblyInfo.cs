75c788bff515e0d3f30dd6bd7bfb230d01d724ad
Rumuel <81417002+Rumuel@users.noreply.github.com>
2026-06-22 22:29:44 +0100

    Debug e testes

--- backend/ITSM/Domain/obj/Debug/net10.0/Domain.AssemblyInfo.cs
+++ backend/ITSM/Domain/obj/Debug/net10.0/Domain.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Domain")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8da3ffce645a636207b734bcd41068dabaa7ef9c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+05acc271ed2ca00cf85709af53827f5bc9f684c0")]
 [assembly: System.Reflection.AssemblyProductAttribute("Domain")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Domain")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
